7b01f538afbb75ed488764ec189d0391796b0a78
Maria <maria.altyeva@gmail.com>
2018-11-29 10:04:04 +0200

    run system tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "check last name capitalization" do
+    user = User.new(last_name: "smith")
+    assert_equal "Smith", user.full_name
+  end
 end
